cd8de4999090a31337d55147631766e67e946ec9
Alexandre <alexmcs@gmail.com>
2022-11-07 16:29:05 -0300

    Configurando o index.js do store.

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // User null by default
     user: null
   },
   mutations: {
+    // User that firebase gives
+    SET_USER (state, user) {
+      state.user = user
+    },
+
+    // Clear user when log out
+    CLEAR_USER (state) {
+      state.user = null
+    }
   },
   actions: {
+    /*async register ({commit}, details) {
+      //
+    },
+    async login ({commit}, details) {
+      //
+    },
+    async logout ({commit}, details) {
+      //
+    }*/
   }
 })
