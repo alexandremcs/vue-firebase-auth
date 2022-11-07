fd83b4bdd2c132485e6eb48324040de82c633c4d
Alexandre <alexmcs@gmail.com>
2022-11-06 22:15:36 -0300

    Configuração do firebase na aplicação

---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/app"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKKu_4YzgKadZ_aMN0ACeUDz0U3_JsqEg",
+  authDomain: "vue-firebase-auth-bab77.firebaseapp.com",
+  projectId: "vue-firebase-auth-bab77",
+  storageBucket: "vue-firebase-auth-bab77.appspot.com",
+  messagingSenderId: "567125563671",
+  appId: "1:567125563671:web:c5c681fd61786d8299bf52"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app)
+
+export { auth }
